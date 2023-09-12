2d7e80f71bf73aa0d277e870651da1187947e925
Tina Chen <tinachen2025@u.northwestern.edu>
2023-09-11 23:09:41 -0700

    angular setup

--- angular-name-conv-proj/README.md
+++ angular-name-conv-proj/README.md
@@ -1,4 +1,4 @@
-# AngularApp
+# AngularNameConvProj
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.2.1.
 
--- angular-name-conv-proj/src/app/app.component.spec.ts
+++ angular-name-conv-proj/src/app/app.component.spec.ts
@@ -12,16 +12,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-app'`, () => {
+  it(`should have as title 'angular-name-conv-proj'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-app');
+    expect(app.title).toEqual('angular-name-conv-proj');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('angular-name-conv-proj app is running!');
   });
 });
